7dd6bca2e772992c0ac44f8965c6ac81e94eb5c8
rituy8100 <rituy8100@naver.com>
2020-12-18 23:44:38 +0900

    알고리즘

--- 구현/게임개발.py
+++ 구현/게임개발.py
@@ -1,26 +1,42 @@
-n,m=map(int,input().split())
-x,y,z=map(int,input().split())
-maps=[]
+n, m = map(int, input().split())
+x, y, z = map(int, input().split())
+maps = []
+d = [[0] * m for _ in range(n)]
 for i in range(n):
     maps.append(list(map(int, input().split())))
 
 for i in range(n):
     print(maps[i])
-maps[x][y]=1
-movx=[-1,0,1,0]
-movy=[0,-1,0,1]
-dir=[0,1,2,3]
-for j in range(4):
+print("시작!!")
+d[x][y] = 1
+movx = [-1, 0, 1, 0]
+movy = [0, -1, 0, 1]
+dir = [0, 1, 2, 3]
+bool = 1
+result = 1
+while bool:
+    count = 0
     for i in range(4):
-        nz=(z+i)%4
-        nx=x+movx[nz]
-        ny=y+movy[nz]
-        if maps[nx][ny]==1:
+        nz = (z + i) % 4
+        nx = x + movx[nz]
+        ny = y + movy[nz]
+        count += 1
+        if maps[nx][ny] == 1 or d[nx][ny] == 1:
+            if count == 4:
+                nx = x - movx[z]
+                ny = y - movy[z]
+                if d[nx][ny] == 1:
+                    x, y = nx, ny
+                    break
+                else:
+                    bool = 0
+                    break
             continue
-        x,y,z=nx,ny,nz
-        maps[x][y]=1
+        x, y, z = nx, ny, nz
+        d[x][y] = 1
+        result += 1
         for k in range(n):
-            print(maps[k])
+            print(d[k])
         print('next')
-        break
 
+print(result)
